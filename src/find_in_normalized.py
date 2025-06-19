600a3d2768050387413560f6c8bcbf9605725e10
Elena Lyulina <elena.lyulina@gmail.com>
2025-06-19 03:29:48 +0200

    fix a problem with looking for numbers

--- src/find_in_normalized.py
+++ src/find_in_normalized.py
@@ -13,10 +13,25 @@ from normalize_text import CONTROLS, HYPHENS, MINUSES, DOUBLE_QUOTES, SINGLE_QUO
 from utils import read_json
 
 
-# TODO:
-#  figure out hot to filter UNK tokens correctly
-#  test on all dataset entries that we can extract normalised answer at least once from the generated answer
-#  handle errors for processing (eka not throwing exceptions but logging them somewhere)
+def safe_char_to_token(tokenized_text, char_index: int, text_length: int, move_right: bool = True):
+    assert 0 <= char_index <= text_length
+    token_idx = tokenized_text.char_to_token(char_index)
+
+    # could be an issue that char_index points at a whitespace so there is actually no token at that place
+    # in that case, it will return None
+    # we then go reft/left until there is some token (should work)
+    def can_move():
+        return (move_right and char_index < text_length) or (not move_right and char_index > 0)
+
+    while can_move() and token_idx is None:
+        if move_right:
+            char_index += 1
+        else:
+            char_index -= 1
+        token_idx = tokenized_text.char_to_token(char_index)
+
+    # if still None, it means that we reached end of string (i.e. char_index == 0 or char_index == text_length+1)
+    return token_idx, char_index
 
 
 # follows srs/normalize_text.py#normalize for compatibility
@@ -25,29 +40,29 @@ def get_normalizers():
     for control in CONTROLS:
         normalizers.append(Replace(control, ''))
 
-    normalizers.append(Replace('\u000b',' '))
-    normalizers.append(Replace('\u000c',' '))
-    normalizers.append(Replace(u'\u0085',' '))
+    normalizers.append(Replace('\u000b', ' '))
+    normalizers.append(Replace('\u000c', ' '))
+    normalizers.append(Replace(u'\u0085', ' '))
 
     for hyphen in HYPHENS | MINUSES:
         normalizers.append(Replace(hyphen, '-'))
     normalizers.append(Replace('\u00ad', ''))
 
     for double_quote in DOUBLE_QUOTES:
-        normalizers.append(Replace(double_quote, '"'))   # \u0022
+        normalizers.append(Replace(double_quote, '"'))  # \u0022
 
     for single_quote in (SINGLE_QUOTES | APOSTROPHES | ACCENTS):
         normalizers.append(Replace(single_quote, "'"))  # \u0027
 
-    normalizers.append(Replace('′', "'"))       # \u2032 prime
-    normalizers.append(Replace('‵', "'"))       # \u2035 reversed prime
-    normalizers.append(Replace('″', "''"))      # \u2033 double prime
-    normalizers.append(Replace('‶', "''"))      # \u2036 reversed double prime
-    normalizers.append(Replace('‴', "'''"))     # \u2034 triple prime
-    normalizers.append(Replace('‷', "'''"))     # \u2037 reversed triple prime
-    normalizers.append(Replace('⁗', "''''"))    # \u2057 quadruple prime
-    normalizers.append(Replace('…', '...'))     # \u2026
-    normalizers.append(Replace(' . . . ', ' ... '))     # \u2026
+    normalizers.append(Replace('′', "'"))  # \u2032 prime
+    normalizers.append(Replace('‵', "'"))  # \u2035 reversed prime
+    normalizers.append(Replace('″', "''"))  # \u2033 double prime
+    normalizers.append(Replace('‶', "''"))  # \u2036 reversed double prime
+    normalizers.append(Replace('‴', "'''"))  # \u2034 triple prime
+    normalizers.append(Replace('‷', "'''"))  # \u2037 reversed triple prime
+    normalizers.append(Replace('⁗', "''''"))  # \u2057 quadruple prime
+    normalizers.append(Replace('…', '...'))  # \u2026
+    normalizers.append(Replace(' . . . ', ' ... '))  # \u2026
 
     for slash in SLASHES:
         normalizers.append(Replace(slash, '/'))
@@ -79,14 +94,11 @@ def get_answer_normalizers():
     return normalizers
 
 
-
 DEFAULT_NORMALIZER = Sequence([
-    normalizers.NFC(),
-    normalizers.NFKC()
-    # ] + get_normalizers())
-] + get_answer_normalizers())
-
-
+                                  normalizers.NFC(),
+                                  normalizers.NFKC()
+                                  # ] + get_normalizers())
+                              ] + get_answer_normalizers())
 
 
 class OffsetNormalizer:
@@ -162,7 +174,8 @@ class OffsetNormalizer:
                 break
 
             # here i know that text_indices[idx] is not UKN
-            assert text_indices[idx] == normalized_text_indices[norm_idx], f"{idx}: {text_indices[idx]}, {normalized_text_indices[norm_idx]}"
+            assert text_indices[idx] == normalized_text_indices[
+                norm_idx], f"{idx}: {text_indices[idx]}, {normalized_text_indices[norm_idx]}"
             norm_to_orig_tokens.append(idx)
             idx += 1
             norm_idx += 1
@@ -176,13 +189,16 @@ class OffsetNormalizer:
 
     def find_in_normalized(self, text: str, substrings: List[str]) -> List[List[Tuple[int, int]]]:
         # we now have the mapping text <-> tokens
-        tokenized_text = self.tokenizer(text=text, return_offsets_mapping=True, return_tensors=None, add_special_tokens=False)
+        tokenized_text = self.tokenizer(text=text, return_offsets_mapping=True, return_tensors=None,
+                                        add_special_tokens=False)
         text_offsets = tokenized_text['offset_mapping']
         text_indices = tokenized_text['input_ids']
 
         # getting normalized text and tokenizing it again to get a mapping normalized_text <-> tokens
         normalized_text = self.tokenizer.decode(text_indices, skip_special_tokens=True)
-        tokenized_normalized_text = self.tokenizer(text=normalized_text, return_offsets_mapping=True, return_tensors=None, add_special_tokens=False)
+        normalized_text_length = len(normalized_text)
+        tokenized_normalized_text = self.tokenizer(text=normalized_text, return_offsets_mapping=True,
+                                                   return_tensors=None, add_special_tokens=False)
         normalized_text_offsets = tokenized_normalized_text['offset_mapping']
         normalized_text_indices = tokenized_normalized_text['input_ids']
 
@@ -192,7 +208,8 @@ class OffsetNormalizer:
         found_text_indices = []
 
         for substring in substrings:
-            tokenized_substring = self.tokenizer(text=substring, return_offsets_mapping=True, return_tensors=None, add_special_tokens=False)
+            tokenized_substring = self.tokenizer(text=substring, return_offsets_mapping=True, return_tensors=None,
+                                                 add_special_tokens=False)
             normalized_substring = self.tokenizer.decode(tokenized_substring['input_ids'], skip_special_tokens=True)
 
             # print(f"Normalized substring: {normalized_substring}")
@@ -201,20 +218,25 @@ class OffsetNormalizer:
                 found_text_indices.append(orig_text_indices)
                 continue
 
-            found_normalized_text_indices = [(m.start(), m.end()) for m in re.finditer(re.escape(normalized_substring), normalized_text)]
+            if normalized_substring.isdigit():
+                # ensure not surrounded by digits or decimal points, otherwise it won't be a GT answer
+                pattern = r'(?<![\d.,])' + re.escape(normalized_substring) + r'(?![\d.,])'
+            else:
+                pattern = re.escape(normalized_substring)
+
+            found_normalized_text_indices = [(m.start(), m.end()) for m in re.finditer(pattern, normalized_text)]
+
             orig_text_indices = []
 
             for substring_start, substring_end in found_normalized_text_indices:
                 # print(substring_start, substring_end, len(normalized_text))
                 # print("Found normalized substring: " + normalized_text[substring_start:substring_end])
-                start_norm_token_idx = tokenized_normalized_text.char_to_token(substring_start)
-                end_norm_token_idx = tokenized_normalized_text.char_to_token(substring_end)
-                # could be an issue that substring_end points at a whitespace so there is actually no token at that place
-                # in that case, it will return None
-                # we then go left until there is some token (should work)
-                while end_norm_token_idx is None:
-                    substring_end -= 1
-                    end_norm_token_idx = tokenized_normalized_text.char_to_token(substring_end)
+                start_norm_token_idx, substring_start = safe_char_to_token(tokenized_normalized_text, substring_start, normalized_text_length, move_right=True)
+                end_norm_token_idx, substring_end = safe_char_to_token(tokenized_normalized_text, substring_end, normalized_text_length, move_right=False)
+
+                # it can be equal if it's a single char token and both substring_start and substring_endpoint at it
+                if (start_norm_token_idx is None) or (end_norm_token_idx is None) or (substring_start > substring_end):
+                    continue
 
                 # to account for UNK tokens
                 start_token_idx = norm_to_orig_tokens[start_norm_token_idx]
@@ -231,25 +253,28 @@ class OffsetNormalizer:
         return found_text_indices
 
 
-
-
 if __name__ == '__main__':
     # idx = 3867
-    # idx = 175
+    idx = 175
+    idx = 3875
+    idx = 272 # GT answer is a number
 
     data = read_json('../data/csnlp/numdoc7_gold_at6_answerless_info_all_extended.json')
     offset_normaliser = OffsetNormalizer()
 
+    # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+
     random_idx = random.sample(range(0, len(data)), k=10)
-    # checkig that it doesnt have any errors for our dataaa
+    # checking that it doesnt have any errors for our dataaa
     for idx in tqdm(range(len(data))):
         example_idx = data[idx]
         generated_string = example_idx['prompt'] + " " + example_idx['generated_answer']
+        # print(generated_string)
         gt_answer = example_idx['answers'][0]
 
         # print(f'GT answers: {example_idx["answers"]}')
+        # print(idx)
         answer_indices = offset_normaliser.find_in_normalized(generated_string, example_idx['answers'])
-
-
-
-
+        # for indices in answer_indices:
+        #     for start, end in indices:
+        #         print(generated_string[start:end])
